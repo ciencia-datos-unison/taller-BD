601c976b26a758c1dbadce49b27cf1a6b2927ce5
juliowaissman <juliowaissman@gmail.com>
2019-03-04 03:53:24 -0700

    Terminado jupyter

---
+++ jupyter/slides/pub-intro.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Convierte la libreta a slides
+
+# jupyter-nbconvert --to slides intro-jupyter.ipynb --reveal-prefix=reveal.js
+jupyter-nbconvert --to slides intro-jupyter.ipynb --reveal-prefix="https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.1.0"
